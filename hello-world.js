fd5e09ca8e194e4e722d02d6c45d5d3475584bd7
danaaraga <danaraga101@gmail.com>
2025-08-26 11:28:58 +0700

    Learn about Variables in js

--- hello-world.js
+++ hello-world.js
@@ -1,2 +1,38 @@
 const text = "Hello, World!";
 console.log(text);
+
+// Variables
+
+// const
+const name1 = "Dana Raga";
+name1 = "Raga Dana"; // This will cause an error
+
+// let
+var name2 = "Dana Raga";
+
+if (true) {
+  var name2 = "Raga Dana"; // This will reassign the variable
+  console.log(name2); // Raga Dana
+}
+
+console.log(name2); // Raga Dana
+
+
+var name3 = "Dana Raga";
+var name3 = "Raga Dana";
+
+if (true) {
+    console.log(name3); // Raga Dana
+}
+
+console.log(name3); // Raga Dana
+
+var name4 = "Dana Raga";
+if (true) {
+    let name4 = "Raga Dana"; // This will create a new variable scoped to the block
+    console.log(name4); // Raga Dana
+}
+
+console.log(name4); // Dana Raga
+
+// End Variables
