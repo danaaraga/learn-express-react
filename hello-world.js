6137b751ee93c4a4c8043cb362784d7f2e1b969e
danaaraga <danaraga101@gmail.com>
2025-08-26 13:21:20 +0700

    Learn about Template Literals or Strings

--- hello-world.js
+++ hello-world.js
@@ -2,7 +2,6 @@ const text = "Hello, World!";
 console.log(text);
 
 // Variables
-
 // const
 const name1 = "Dana Raga";
 name1 = "Raga Dana"; // This will cause an error
@@ -36,3 +35,16 @@ if (true) {
 console.log(name4); // Dana Raga
 
 // End Variables
+// Template Literals or Template Strings
+
+let name5 = "Dana Raga";
+let gender = "Laki-laki";
+
+console.log(name5 + "dan saya berjenis kelamin: " + gender);
+
+let name6 = "Dana Raga";
+let gender1 = "Laki-laki";
+
+console.log(`${name6} dan saya berjenis kelamin: ${gender1}`);
+
+// End Template Literals or Strings
